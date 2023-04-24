21ee58dc8a0edf3e76cab80774cb929b130da19c
kgpksh <tkdgns543@gmail.com>
2023-04-24 15:18:53 +0900

    Hello 만들기

---
+++ src/test/java/com/docker_spring/example/docker_spring/DockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker_spring.example.docker_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
